bcd7c4990572bab64ae75265a3b2f27e856d8db4
GermanDG6 <germandg6@gmail.com>
2021-08-20 15:04:11 +0100

    add file

---
+++ challenge1/multiplicationTable.js
@@ -0,0 +1,9 @@
+const multiplicationTable = (number) => {
+  let result = `Tabla del ${number} \n`;
+  for (let i = 1; i <= 10; i++) {
+    result += `${i} x ${number} = ${i * number} \n`;
+  }
+  return result;
+};
+
+console.log(multiplicationTable(0));
