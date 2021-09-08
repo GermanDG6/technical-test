6290036a9b02e074a9019ef9f90299009ef27a00
GermanDG6 <germandg6@gmail.com>
2021-09-08 16:43:37 +0100

    Convert result in a number

--- challenge8/reverseNumber.js
+++ challenge8/reverseNumber.js
@@ -1,5 +1,7 @@
 const reverseNumber = (number) => {
-  return number.toFixed().split("").reverse().join().replace(/[,]/gi, "");
+  return Number(
+    number.toFixed().split("").reverse().join().replace(/[,]/gi, "")
+  );
 };
 
 console.log(reverseNumber(1000002));
