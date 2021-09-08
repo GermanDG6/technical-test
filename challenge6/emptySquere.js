33d7a0dc830ab4e6fe79903762e6b3dadb69ead4
GermanDG6 <germandg6@gmail.com>
2021-09-08 15:18:01 +0100

    Excersice 6 done

---
+++ challenge6/emptySquere.js
@@ -0,0 +1,33 @@
+const horizontalLines = (size) => {
+  let horizontal = "";
+  for (let i = 0; i < size; i++) {
+    horizontal += "*";
+  }
+  return horizontal;
+};
+
+const verticalLines = (size) => {
+  let vertical = "";
+  for (let i = 0; i <= size; i++) {
+    vertical.length === 0
+      ? (vertical += "*")
+      : vertical.length === size - 1
+      ? (vertical += "*")
+      : vertical.length > 0
+      ? (vertical += " ")
+      : vertical;
+  }
+  return vertical;
+};
+
+const makeSquare = (size) => {
+  let square = "";
+  square += horizontalLines(size) + "\n";
+  for (let i = 1; i <= size - 2; i++) {
+    square += verticalLines(size) + "\n";
+  }
+  square += horizontalLines(size);
+  return square;
+};
+
+console.log(makeSquare(4));
