b2d473fffea13282b99ef2d9405f53edf20dfc65
GermanDG6 <germandg6@gmail.com>
2021-09-10 12:55:08 +0100

    Add prototype function

--- challenge14/repeatString.js
+++ challenge14/repeatString.js
@@ -1,3 +1,4 @@
+//Classic Function
 const repeatString = (str, times) => {
   let repeatedString = "";
   for (let i = 0; i < times; i++) {
@@ -7,3 +8,14 @@ const repeatString = (str, times) => {
 };
 
 console.log(repeatString("hola", 3));
+
+//Prototype function
+String.prototype.repeatStr = function (times) {
+  let repeatedString = "";
+  for (let i = 0; i < times; i++) {
+    repeatedString += this;
+  }
+  return repeatedString;
+};
+
+console.log("German D G ".repeatStr(5));
