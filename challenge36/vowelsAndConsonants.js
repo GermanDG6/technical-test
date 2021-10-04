aabaa1029896dd18dcf7985a8178dba704eddf30
GermanDG6 <germandg6@gmail.com>
2021-10-04 11:54:33 +0100

    Exercise 36 done

---
+++ challenge36/vowelsAndConsonants.js
@@ -0,0 +1,17 @@
+const vowelsAndConsonants = (text) => {
+  let vocals = 0;
+  let consonants = 0;
+  letters = text
+    .split("")
+    .filter((letter) => /[áéíóú\w]/gi.test(letter) && isNaN(letter));
+  for (let i = 0; i < letters.length; i++) {
+    if (/[áéíóúaeiou]/gi.test(letters[i])) {
+      vocals++;
+    } else {
+      consonants++;
+    }
+  }
+  return `EL texto tiene ${vocals} vocales y ${consonants} consonantes`;
+};
+
+console.log(vowelsAndConsonants("GermÁn  Delgado GarCÍa 123 ,.klasdfh "));
