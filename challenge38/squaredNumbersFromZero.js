0d5022d58812ade21fa801974405939abed7c3ce
GermanDG6 <germandg6@gmail.com>
2021-10-06 13:31:00 +0100

    :recycle: Refactored function

--- challenge38/squaredNumbersFromZero.js
+++ challenge38/squaredNumbersFromZero.js
@@ -1,9 +1,21 @@
+// const squaredNumbersFromZero = (number) => {
+//   let squaredNumbers = [];
+//   for (let i = 0; i <= number; i++) {
+//     squaredNumbers.push(Math.pow(i, 2));
+//   }
+//   return squaredNumbers;
+// };
+
+const squaredNumbers = (number) => {
+  return number * number;
+};
+
 const squaredNumbersFromZero = (number) => {
-  let squaredNumbers = [];
+  let squaredList = [];
   for (let i = 0; i <= number; i++) {
-    squaredNumbers.push(Math.pow(i, 2));
+    squaredList.push(squaredNumbers(i));
   }
-  return squaredNumbers;
+  return squaredList;
 };
 
 module.exports = squaredNumbersFromZero;
